01762cbea5a31c813a1dca54a4ccda05de236e50
DESKTOP-3QIQSGO\User <a6010521@gmail.com>
2025-02-19 03:30:29 +0800

    更新代碼

--- 06_finance_info.py
+++ 06_finance_info.py
@@ -1,40 +1,30 @@
 import requests
-import pandas as pd
 from bs4 import BeautifulSoup
 
-# Yahoo 股市產業類股網址
-url = "https://tw.stock.yahoo.com/class-quote?sectorId=40&exchange=TAI"
+def get_finaace():
 
-# 設定 headers 模擬瀏覽器請求
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
-}
+    url = "https://tw.stock.yahoo.com/class-quote?sectorId=40&exchange=TAI"
+    headers = {
+        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
+    }
+    
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    
+    if response.status_code == 200:
+        print("good")
+        div_element = soup.find_all("div", class_="Lh(20px) Fw(600) Fz(16px) Ell")
+        
+        for div_elements in div_element:
+            title = div_elements.text
+            print(title)
+       
 
-# 發送請求
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, "html.parser")
-# 找到表格
-table = soup.find("table")
-if table:
-    # 取得表頭
-    headers = [th.text.strip() for th in table.find("thead").find_all("th")]
-    
-    # 取得表格內容
-    rows = []
-    for tr in table.find("tbody").find_all("tr"):
-        cells = [td.text.strip() for td in tr.find_all("td")]
-        rows.append(cells)
-    
-    # 轉成 DataFrame
-    df = pd.DataFrame(rows, columns=headers)
-    
-    # 顯示 DataFrame
-    print(df)
-else:
-    print("找不到表格")
-    print(response.status_code)
-
+    else:
+        print("fail")
 
     
 
     
+get_finaace()
